3574d2181e8b5fb022f5820a8d167bc386c4db5c
bergmann <info@bergmann89.de>
2018-09-25 21:33:53 +0200

    * refactored cmake files

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,1 +1,6 @@
-Option                      ( CPPUTILS_BUILD_SHARED "Build cpputils shared library" OFF )
+Option ( CPPUTILS_BUILD_SHARED
+         "Build cpputils shared library"
+         ON )
+Option ( CPPUTILS_INSTALL_DEV_FILES
+         "Install development files of cpputils"
+         ON )
